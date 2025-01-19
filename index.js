660cec4713bb74e6d819ca7b649c0949f0916258
Rasadul Islam <rasadulislam.cse@gmail.com>
2025-01-19 21:59:37 +0600

    delete users from database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzkk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,12 @@ async function run() {
     const feedbackCollection = client.db('eduLoopDb').collection('feedback');
 
     // Users related api
+    // get user info
+    app.get('/users', async(req, res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    // post user info
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
@@ -40,6 +46,13 @@ async function run() {
         return res.send({ massage: "users email already added", insertedId: null })
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+    // delete use info
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query= {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
